230b09c2a86f59e72d6afeedc28650ebc6add987
DESKTOP-R5OS0DA\junki <2pcolor@gmail.com>
2018-04-01 21:49:05 +0900

    レッスン４課題　
    配列の表示を逆順に表示する

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+void Start()
+    {
+        int[] array = {10,20,30,40,50};
+        
+
+        for(int i=4; 0 <=i; i--)
+        {
+            Debug.Log (array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        }
+    }
